e9e5b646c0d3ef964ead1b187fe6f9e59aa4e7f3
alokshakya <alokshakyacode@gmail.com>
2019-11-13 12:42:27 +0530

    config file added for security url and other resources use

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb+srv://test1_user:test1_pass@cluster0-dhu1i.mongodb.net/jwtApp',
+    'facebook': {
+        clientId:'329999474206572',
+        clientSecret:'56503eed5402bd290d95be895f5ca89d'
+    }
+}
+
+//'mongoUrl' : 'mongodb://localhost:27017/conFusion',
