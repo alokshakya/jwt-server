d6ab17140939bfc41c772b6c9cc2ca497b0183b0
alokshakya <alokshakyacode@gmail.com>
2019-11-14 16:20:30 +0530

    added custum password validation using passport-local-mongoose instance API authenticate(password, [cb])

--- routes/users.js
+++ routes/users.js
@@ -52,13 +52,70 @@ router.post('/signup', function( req, res, next){
   });
 });
 
-router.post('/login', passport.authenticate('local',{session:false}), (req,
-   res, next) => {
+// router.post('/login', passport.authenticate('local',{session:false}), (req,
+//    res, next) => {
   
-  var token = authenticate.getToken({_id: req.user._id});
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, token: token, status : 'You are Successfully logged in!'});   
+//   var token = authenticate.getToken({_id: req.user._id});
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'application/json');
+//   res.json({success: true, token: token, status : 'You are Successfully logged in!'});   
+// });
+
+router.post('/login', (req, res, next) => {
+ User.findOne({username:req.body.username},(err,user) =>
+ {
+   if(err)
+   {
+    res.statusCode = 500;
+    res.setHeader('Content-Type','application/json');
+    res.json({  err: err});
+    return;
+   }
+   if(!user)
+   {
+    res.statusCode = 500;
+    res.setHeader('Content-Type','application/json');
+    res.json({  err: 'Username not found'});
+    return;
+   }
+   if(user)
+   {
+     user.authenticate(req.body.password, (err,thisModel, passErr) =>{
+      if(passErr)
+      {
+        console.log('passErr object :');
+        console.log(passErr);
+        res.statusCode = 500;
+        res.setHeader('Content-Type','application/json');
+        res.json({  err: passErr});
+        return;
+      }
+      if(err !==null)
+      {
+        console.log('err object :');
+        console.log(err);
+        res.statusCode = 500;
+        res.setHeader('Content-Type','application/json');
+        res.json({  err: {name:"passwordMissing",message:"No Password Provided"}});
+        return;
+      }
+      if(thisModel)
+      {
+        console.log('thisModel Object print :');
+        console.log(thisModel);// thisModel contains fileds salt hash apart from other User Attributes
+        var token = authenticate.getToken({_id: user._id});
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({success: true, token: token, status : 'You are Successfully logged in!'});  
+      }
+      
+     })
+   }
+ })
+//  var token = authenticate.getToken({_id: req.user._id});
+//  res.statusCode = 200;
+//  res.setHeader('Content-Type', 'application/json');
+//  res.json({success: true, token: token, status : 'You are Successfully logged in!'});   
 });
 
 
