4da9af99fdf773f2268a3616f32889a26f90fa6f
alokshakya <alokshakyacode@gmail.com>
2019-11-14 12:19:33 +0530

    new database url changed to connect to atlas database

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb+srv://test1_user:test1_pass@cluster0-dhu1i.mongodb.net/jwtApp',
+    'mongoUrl' : 'mongodb+srv://jwtAppUser:jwtAppPass@cluster0-do7xv.mongodb.net/test?retryWrites=true&w=majority',
     'facebook': {
         clientId:'329999474206572',
         clientSecret:'56503eed5402bd290d95be895f5ca89d'
@@ -8,3 +8,4 @@ module.exports = {
 }
 
 //'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+//mongodb+srv://jwtAppUser:jwtAppPass@cluster0-do7xv.mongodb.net/test?retryWrites=true&w=majority
